14cb96cc0e59cd7c4f679ef691f05d5ef4d4bac7
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-11-25 14:27:17 -0500

    Finished lecture

--- server/index.js
+++ server/index.js
@@ -2,6 +2,26 @@ const express = require('express')
 const usersCtrl = require('./usersController')
 const app = express()
 
+app.use(express.json())
+
+app.get('/api/users', usersCtrl.getAllUsers)
+app.get('/api/users/:user_id', usersCtrl.getSingleUser)
+app.post('/api/users', usersCtrl.addUser)
+app.put('/api/users/:user_id', usersCtrl.editUser)
+app.delete('/api/users/:user_id', usersCtrl.deleteUser)
+
+app.listen(4554, () => console.log('Server listening on port 4554'))
+
+/*
+axios.get('/api/users').then(res => {≠
+  this.setState({
+    messages: res.data
+  })
+}).catch(err => {
+  //handle the err however
+})
+*/
+
 app.get('/', (req, res) => {
   res.send('THE HOMEPAGE')
 })
@@ -13,18 +33,3 @@ app.get('/test', (req, res) => {
 app.get('/reject', (req, res) => {
   res.status(404).send('I DO NOT HAVE THAT')
 })
-
-app.get('/api/users', usersCtrl.getAllUsers)
-app.get('/api/users/:user_id', usersCtrl.getSingleUser)
-
-app.listen(4554, () => console.log('Server listening on port 4554'))
-
-/*
-axios.get('/api/users').then(res => {
-  this.setState({
-    messages: res.data
-  })
-}).catch(err => {
-  //handle the err however
-})
-*/
--- server/usersController.js
+++ server/usersController.js
@@ -1,4 +1,5 @@
 const users = require('../data.json')
+let id = users[users.length - 1].id + 1
 
 module.exports = {
   getAllUsers: (req, res) => {
@@ -15,6 +16,7 @@ module.exports = {
     }
   },
   getSingleUser: (req, res) => {
+    //req.params.user_id
     const { user_id } = req.params
 
     const user = users.find((element) => element.id === +user_id)
@@ -24,5 +26,55 @@ module.exports = {
     } else {
       res.status(404).send('User does not exist')
     }
+  },
+  addUser: (req, res) => {
+    const { first_name, last_name, email } = req.body
+
+    const newUser = {
+      id,
+      first_name,
+      last_name,
+      email,
+    }
+
+    users.push(newUser)
+
+    id++
+
+    res.status(200).send(users)
+  },
+  editUser: (req, res) => {
+    const { user_id } = req.params
+    const { first_name, last_name, email } = req.body
+
+    const index = users.findIndex((element) => element.id === +user_id)
+
+    if (index === -1) {
+      return res.status(404).send('User does not exist')
+    }
+
+    const existingUser = users[index]
+
+    users[index] = {
+      id: existingUser.id,
+      first_name: first_name || existingUser.first_name,
+      last_name: last_name || existingUser.last_name,
+      email: email || existingUser.email,
+    }
+
+    res.status(200).send(users)
+  },
+  deleteUser: (req, res) => {
+    const { user_id } = req.params
+
+    const index = users.findIndex((element) => element.id === +user_id)
+
+    if (index === -1) {
+      return res.status(404).send('User does not exist')
+    }
+
+    users.splice(index, 1)
+
+    res.status(200).send(users)
   },
 }
